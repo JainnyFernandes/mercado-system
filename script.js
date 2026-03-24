55cb97633b0a13e64c908ef1792c248a21454e29
Jainny Fernandes <150845153+JainnyFernandes@users.noreply.github.com>
2026-03-24 07:56:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-let lista = document.getElementById("lista");
-
 function adicionar() {
-  let input = document.getElementById("produto");
-
-  if (input.value === "") {
-    alert("Digite um produto!");
-    return;
-  }
-
-  let item = document.createElement("li");
-  item.innerText = input.value;
-
-  lista.appendChild(item);
-
-  input.value = "";
+  alert("funcionou");
+}
 }
